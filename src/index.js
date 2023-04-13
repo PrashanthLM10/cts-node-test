c4c0c435242819e06bbbca8942295519f7b8c9c2
Prashanth <prashanth.sunny0236@gmail.com>
2023-04-13 15:08:41 +0530

    Added CORS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 global.messageId = 123;
 global.print = console.log;
 const express = require('express');
+const cors = require('cors');
 const messageRoutes = require('./routers/message.routes');
 
 require('./db/mongoose');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(messageRoutes);
 
