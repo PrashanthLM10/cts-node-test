fe93fc608fa7b5cd0c9749001f0e46993304d78b
Prashanth <prashanth.sunny0236@gmail.com>
2023-09-23 00:26:58 +0530

    Added previous message feature

---
+++ src/models/previousMessage.model.js
@@ -0,0 +1,21 @@
+// create Task Model
+
+const mongoose = require("mongoose");
+
+
+const PreviousMessage = new mongoose.Schema({
+    _id: {
+        type:Number,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+}, {
+    timestamps: true,
+    'collection': 'PreviousMessage'
+})
+
+module.exports = {PreviousMessageModel: mongoose.model('PreviousMessage', PreviousMessage), prevMessageFields: ['message','_id']};
--- src/routers/message.routes.js
+++ src/routers/message.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const {MessageModel, messageFields} = require('../models/message.model');    
+const {PreviousMessageModel, prevMessageFields} = require('../models/previousMessage.model');    
 
 
 //get routes
@@ -10,7 +11,18 @@ router.get('/message/getMessage/:id', async (req, res) => {
         const id = req.params.id;
         print(id);
         const [messageObj] = await MessageModel.find({_id:messageId} );
-        res.status(200).send(messageObj);
+        const [previousMessageObj] = await PreviousMessageModel.find({_id:messageId} );
+        if(previousMessageObj) {
+            messageObj.previousMessage = previousMessageObj.message;
+            messageObj.previousMessageTime = previousMessageObj.updatedAt;
+        }
+        const response = {
+            message: messageObj.message,
+            time: messageObj.updatedAt,
+            previousMessage: previousMessageObj?.message || '',
+            previousMessageTime: previousMessageObj?.updatedAt || ''
+        }
+        res.status(200).send(response);
     } catch (e) {res.status(500).send(e)};
 });
 
@@ -19,11 +31,20 @@ router.post('/message/setMessage', async (req, res) => {
     try {
         const {message} = {...req.body};
         const [messageObj] = await MessageModel.find({_id:messageId} );
+        const [prevMessageObj] = await PreviousMessageModel.find({_id:messageId} );
         if(messageObj) {
+            let prevMsgSaveStatus;
+                if(prevMessageObj) {
+                    prevMessageObj.message = messageObj.message;
+                    prevMessageObj._id = messageId;
+                    prevMsgSaveStatus = await prevMessageObj.save();
+                } else {
+                    prevMsgSaveStatus = await new PreviousMessageModel({_id: messageId, message: message}).save();
+                }
                 messageObj._id = messageId;
                 messageObj.message = message;
                 const saveStatus = await messageObj.save();
-                res.status( 200).send( saveStatus);
+                res.status( 200).send({prevMsgSaveStatus, saveStatus});
         } else {
             const saveStatus = await new MessageModel({_id: messageId, message: message}).save();
             res.status( 200).send( saveStatus);
