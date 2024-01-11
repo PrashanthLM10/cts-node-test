19cccb3fa948315329b7bc1b7bfe275174ffffb8
Prashanth Kumar <prashanth.sunny0236@gmail.com>
2024-01-11 15:42:51 +0530

    Changed Mongodb user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionURL = 'mongodb+srv://prashanth-mongo:Iaamjfs4y!@prashanth-mongo-cluster.sauezk4.mongodb.net/CTS-Node-test';
+const connectionURL = 'mongodb+srv://prashanthsunny0236:G34qzW5v7oz0wsfc@messagecluster.0ysmova.mongodb.net/MessageDB?retryWrites=true&w=majority';
 
 const MONGODB_URL = 'mongodb+srv://prashanth_kumar_node:sanmongodb@prashanth-node.tzaoh.mongodb.net/Prashanth-Node?retryWrites=true&w=majority';
 
